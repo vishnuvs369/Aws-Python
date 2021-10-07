8c5c6e5dfb0522574fdb2c82c256122d59e3e602
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-07 10:38:02 +0530

    added S3 trigger

---
+++ Aws-S3-Trigger-Lambda-json/lambda_function.py
@@ -0,0 +1,16 @@
+import json
+   
+# Opening JSON file
+f = open('data.json',)
+   
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+   
+# Iterating through the json
+# list
+print(data)
+
+   
+# Closing file
+f.close()
