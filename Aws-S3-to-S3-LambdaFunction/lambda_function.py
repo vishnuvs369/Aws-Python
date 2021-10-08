1ef779203ba31c0b04ee0e801ef0fdc94a20f784
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-09 01:17:46 +0530

    added Aws s3 to s3

--- Aws-S3-to-S3-LambdaFunction/lambda_function.py
+++ Aws-S3-to-S3-LambdaFunction/lambda_function.py
@@ -1,14 +1,28 @@
+import json
 import boto3
+import urllib
 
 s3 = boto3.resource('s3')
 clientname=boto3.client('s3')
 def lambda_handler(event, context):
     bucket = 'initial369'
+    key = event['Records'][0]['s3']['object']['key']
+    key = urllib.parse.unquote_plus(key, encoding='utf-8')
+    
+    message = 'hey this file got uploaded ' + key + ' to this bucket' + bucket
+    print (message)
+    
+    response = clientname.get_object(Bucket=bucket,Key=key)
+    contents = response["Body"].read().decode()
+    contents = json.loads(contents)
+    print("these are the contents of the file : \n" , contents)
+    
     try:
         response = clientname.list_objects(
             Bucket=bucket,
             MaxKeys=5
         )
+        
 
         for record in response['Contents']:
             key = record['Key']
