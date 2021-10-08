aaef813f5c35158b22e89600be888ed24f508ccd
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-08 20:15:26 +0530

    updated code

--- AWS-Secrets-Manager-and-Lambda/lambda_function.py
+++ AWS-Secrets-Manager-and-Lambda/lambda_function.py
@@ -2,16 +2,16 @@ import json
 import boto3
 import urllib3
 
-REGION = "eu-central-1"
-SECRET_NAME = "lambda-authorizer-api-key"
-API_URL = "https://z785cwhbu3.execute-api.eu-central-1.amazonaws.com/demo/birds"
-BIRD_ID = "079b42b8-a1ab-11eb-bcbc-0242ac130002"
+REGION = "us-east-1"
+SECRET_NAME = "mygithubkey"
+API_URL = "https://api.github.com/************"
+
 
 def lambda_handler(event, context):
   
   http = urllib3.PoolManager()
   api_key = get_api_key()
-  response = http.request('GET', f"{API_URL}/{BIRD_ID}", headers=api_key)
+  response = http.request('GET', f"{API_URL}", headers=api_key)
 
   if response.status != 200:
     return {
