a27be1d823e886d2cabdd3ee9686dc41fa5116d4
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-09 00:58:36 +0530

    added Aws s3 to s3

---
+++ Aws-S3-to-S3-LambdaFunction/lambda_function.py
@@ -0,0 +1,30 @@
+import boto3
+
+s3 = boto3.resource('s3')
+clientname=boto3.client('s3')
+def lambda_handler(event, context):
+    bucket = 'initial369'
+    try:
+        response = clientname.list_objects(
+            Bucket=bucket,
+            MaxKeys=5
+        )
+
+        for record in response['Contents']:
+            key = record['Key']
+            copy_source = {
+                'Bucket': bucket,
+                'Key': key
+            }
+            try:
+                destbucket = s3.Bucket('final369')
+                destbucket.copy(copy_source, key)
+                print('{} transferred to destination bucket'.format(key))
+
+            except Exception as e:
+                print(e)
+                print('Error getting object {} from bucket {}. '.format(key, bucket))
+                raise e
+    except Exception as e:
+        print(e)
+        raise e
