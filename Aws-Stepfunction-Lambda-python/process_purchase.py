64c0ba007452f1f238b07b2eef43898a1f4bb77b
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-09 12:00:15 +0530

    added stepfunction

---
+++ Aws-Stepfunction-Lambda-python/process_purchase.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+import datetime
+
+print('Loading message function...')
+
+def process_purchase(message, context):
+
+    #1. Log input message
+    print('Received message from Step Function:')
+    print(message)
+    
+    #2. Construct response
+    response = {}
+    response['TransactionType'] = message['TransactionType']
+    response['Timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    response['Message'] = 'Hello from Purchase Processor!'
+    
+    #3. Return response
+    print(response)
+    
+    return (response)
