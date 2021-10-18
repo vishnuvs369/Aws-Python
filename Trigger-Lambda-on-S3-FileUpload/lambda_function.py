944292721b1b540579aba9b1c91869283f79a2a6
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-18 17:05:48 +0530

    updated

--- Trigger-Lambda-on-S3-FileUpload/lambda_function.py
+++ Trigger-Lambda-on-S3-FileUpload/lambda_function.py
@@ -2,9 +2,9 @@ import json
 import boto3
 import urllib
 
+s3_client = boto3.client('s3')
+
 def lambda_handler (event, context):
-    s3_client = boto3.client('s3')
-    
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     key = urllib.parse.unquote_plus(key, encoding='utf-8')
